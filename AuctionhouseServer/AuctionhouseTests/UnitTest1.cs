bbfeaf40e5edcf8c780e4d9b5ba485e17f2fa4cd
Daniel Alexander Nedergaard <DanielxNedergaard@hotmail.dk>
2016-09-07 08:46:48 +0200

    Added 3 projects

---
+++ AuctionhouseServer/AuctionhouseTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AuctionhouseTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
