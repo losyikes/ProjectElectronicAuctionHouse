76289e9bd648a5e31970247affd771477b06e55c
morten_clausen <mortenclausen@gmail.com>
2016-09-10 12:30:43 +0200

    Added IScreen interface and Screen class

---
+++ AuctionhouseServer/AuctionhouseServer/IScreen.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuctionhouseServer
+{
+    interface IScreen
+    {
+        void Print(string input);
+        void PrintLine(string input);
+    }
+}
---
+++ AuctionhouseServer/AuctionhouseServer/Screen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuctionhouseServer
+{
+    class Screen : IScreen
+    {
+        public void Print(string input)
+        {
+            Console.WriteLine(input);
+        }
+        public void PrintLine(string input)
+        {
+            Console.WriteLine(input);
+        }
+    }
+}
